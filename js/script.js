4ea1aa6c73942b9f125bf429dc9687db4cf42288
moznion <moznion@gmail.com>
2013-02-20 17:42:08 +0900

    Improve append behavior.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 $(function(){
     $(document).bind('mouseover', function() {
-        if ($('ul.export-references li').length < 5) { // XXX
+        if ($('ul.export-references li').length < 5) { // XXX EVIL!!!
             $('ul.export-references li').each(function(i) {
-                var baseUri, httpsUri, sshUri, gitUri;
+                var baseUri, httpsUri, sshUri, gitUri, delayTime;
                 if (i === 1) {
                     baseUri  = $(this).context.baseURI;
                     httpsUri = baseUri.replace(/(.*):\/\/(.*)\/.*?\/(.*)$/, "$1://$2/$3.git");
                     sshUri   = httpsUri.replace(/.*:\/\/(.*)\/(.*)/, "git@$1:$2");
                     gitUri   = httpsUri.replace(/.*\/\/(.*)/, "git://$1");
 
-                    // I want more cool view behavior...
-                    $($(this)).after(constructListElement('Git Read-Only', gitUri)).hide().fadeIn(1000);
-                    $($(this)).after(constructListElement('SSH', sshUri)).hide().fadeIn(1000);
+                    delayTime = 500;
+                    $((constructListElement('SSH', sshUri))).hide().appendTo($(this)).fadeIn(delayTime);
+                    $((constructListElement('Git Read-Only', gitUri))).hide().appendTo($(this)).fadeIn(delayTime);
 
                     return;
                 }
