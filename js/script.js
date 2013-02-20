dce9e2c4d10b4381bcae814f79d192e77fb684b1
moznion <moznion@gmail.com>
2013-02-20 17:53:38 +0900

    Extracted function to construct git uri.

--- js/script.js
+++ js/script.js
@@ -2,22 +2,27 @@ $(function(){
     $(document).bind('mouseover', function() {
         if ($('ul.export-references li').length < 5) { // XXX EVIL!!!
             $('ul.export-references li').each(function(i) {
-                var baseUri, httpsUri, sshUri, gitUri, delayTime;
+                var uris, delayTime = 500;
                 if (i === 1) {
-                    baseUri  = $(this).context.baseURI;
-                    httpsUri = baseUri.replace(/(.*):\/\/(.*)\/.*?\/(.*)$/, "$1://$2/$3.git");
-                    sshUri   = httpsUri.replace(/.*:\/\/(.*)\/(.*)/, "git@$1:$2");
-                    gitUri   = httpsUri.replace(/.*\/\/(.*)/, "git://$1");
-
-                    delayTime = 500;
-                    $((constructListElement('SSH', sshUri))).hide().appendTo($(this)).fadeIn(delayTime);
-                    $((constructListElement('Git Read-Only', gitUri))).hide().appendTo($(this)).fadeIn(delayTime);
+                    uris = constructUri($(this).context.baseURI);
+                    $((constructListElement('SSH', uris.ssh))).hide().appendTo($(this)).fadeIn(delayTime);
+                    $((constructListElement('Git Read-Only', uris.git))).hide().appendTo($(this)).fadeIn(delayTime);
 
                     return;
                 }
             });
         }
     });
+
+    function constructUri(baseUri) {
+        var httpsUri, sshUri, gitUri;
+
+        httpsUri = baseUri.replace(/(.*):\/\/(.*)\/.*?\/(.*)$/, "$1://$2/$3.git");
+        sshUri   = httpsUri.replace(/.*:\/\/(.*)\/(.*)/, "git@$1:$2");
+        gitUri   = httpsUri.replace(/.*\/\/(.*)/, "git://$1");
+
+        return {'ssh': sshUri, 'git': gitUri};
+    }
 
     function constructListElement(protocol, uri) {
         return '<li>' +
@@ -27,6 +32,6 @@ $(function(){
             '</label>' +
             '<input type="text" readonly=" spellcheck="false" class="url-field js-url-field" name="link-field" ' +
             'value=' + uri + '>' +
-        '</li>';
+            '</li>';
     }
 });
