a61d114a524b5f62053684d82fcb281a1ce36661
moznion <moznion@gmail.com>
2013-02-20 17:33:04 +0900

    Implement basic function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
 $(function(){
-    var url, ssh, old_url;
-
-    url = $(location).attr('href');
-    old_url = undefined;
-
-
     $(document).bind('mouseover', function() {
         if ($('ul.export-references li').length < 5) { // XXX
-            ssh = '<li><label for="link-field"><strong>AAAAAAA!!!</strong> this gist</label><input type="text" readonly=" spellcheck="false" class="url-field js-url-field" name="link-field" value="https://gist.github.com/Shinpeim/4982776"></li>';
             $('ul.export-references li').each(function(i) {
+                var baseUri, httpsUri, sshUri, gitUri;
                 if (i === 1) {
-                    // console.log($(this).context.baseURI);
-                    $($(this)).after(ssh);
+                    baseUri  = $(this).context.baseURI;
+                    httpsUri = baseUri.replace(/(.*):\/\/(.*)\/.*?\/(.*)$/, "$1://$2/$3.git");
+                    sshUri   = httpsUri.replace(/.*:\/\/(.*)\/(.*)/, "git@$1:$2");
+                    gitUri   = httpsUri.replace(/.*\/\/(.*)/, "git://$1");
+
+                    // I want more cool view behavior...
+                    $($(this)).after(constructListElement('Git Read-Only', gitUri)).hide().fadeIn(1000);
+                    $($(this)).after(constructListElement('SSH', sshUri)).hide().fadeIn(1000);
+
+                    return;
                 }
             });
         }
     });
+
+    function constructListElement(protocol, uri) {
+        return '<li>' +
+            '<label for="link-field">' +
+            '<strong>clone</strong> ' +
+            'this gist (' + protocol +')' +
+            '</label>' +
+            '<input type="text" readonly=" spellcheck="false" class="url-field js-url-field" name="link-field" ' +
+            'value=' + uri + '>' +
+        '</li>';
+    }
 });
