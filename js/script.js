4e0f642c753e06f1957baa4383f5bc85bd96ad21
Kotaro Kawashima <utageyanagi+kkgithub@gmail.com>
2013-11-13 01:51:15 +0900

    OnDemandJavascript!
    http://qiita.com/Quramy@github/items/bdaa3e199eee917e7c79

--- js/script.js
+++ js/script.js
@@ -1,43 +1,45 @@
-var script = function() {
-    var appendList = function(){
+document.addEventListener("DOMContentLoaded", function(){
+    var Gist = function (baseURL, protocol, description) {
+        if (!Gist.prototype.httpsURL) {
+            Gist.prototype.httpsURL = baseURL.replace(/(.*):\/\/(.*)\/.*?\/(.*)$/, "$1://$2/$3.git");
+        }
 
-        var Gist = function (baseURL, protocol, description) {
-            if (!Gist.prototype.httpsURL) {
-                Gist.prototype.httpsURL = baseURL.replace(/(.*):\/\/(.*)\/.*?\/(.*)$/, "$1://$2/$3.git");
-            }
+        var url = '';
+        if (protocol === 'ssh') {
+            url = this.httpsURL.replace(/.*:\/\/(.*)\/(.*)/, "git@$1:$2");
+        } else if (protocol === 'git') {
+            url = this.httpsURL.replace(/.*\/\/(.*)/, "git://$1");
+        }
 
-            var url = '';
-            if (protocol === 'ssh') {
-                url = this.httpsURL.replace(/.*:\/\/(.*)\/(.*)/, "git@$1:$2");
-            } else if (protocol === 'git') {
-                url = this.httpsURL.replace(/.*\/\/(.*)/, "git://$1");
-            }
-
-            this.urlInfo = {
-                protocol:    protocol,
-                description: description,
-                url:         url
-            };
+        this.urlInfo = {
+            protocol:    protocol,
+            description: description,
+            url:         url
         };
+    };
 
-        Gist.prototype.constructList = function () {
-            var urlInfo = this.urlInfo;
+    Gist.prototype.constructList = function () {
+        var urlInfo = this.urlInfo;
 
-            var list_template = '<li class="_gistmoreurl">' +
-                '<label for="link-field">' +
-                '<strong>clone</strong> ' +
-                'this gist (<%- description %>)' +
-                '</label>' +
-                '<input type="text" readonly=" spellcheck="false" class="url-field js-url-field" name="link-field" ' +
-                'value=<%- url %>>' +
-                '</li>';
-            var list = list_template.replace(/<%- description %>/, urlInfo.description)
-                                    .replace(/<%- url %>/,         urlInfo.url);
+        var list_template = '<li>' +
+            '<label for="link-field">' +
+            '<strong>clone</strong> ' +
+            'this gist (<%- description %>)' +
+            '</label>' +
+            '<input type="text" readonly=" spellcheck="false" class="url-field js-url-field" name="link-field" ' +
+            'value=<%- url %>>' +
+            '</li>';
+        var list = _.template(list_template)({
+            description: urlInfo.description,
+            url:         urlInfo.url
+        });
 
-            return list;
-        };
+        return list;
+    };
 
+    function appendList(){
         var $urlList  = $('ul.export-references li');
+
         if ($urlList.length > 0 && $('_gistmoreurl').length == 0) {
             var $targetLi = $($urlList[1]);
             var $baseURI;
@@ -53,14 +55,9 @@ var script = function() {
             $(sshGist.constructList()).hide().appendTo($targetLi).fadeIn(delayTime);
             $(gitGist.constructList()).hide().appendTo($targetLi).fadeIn(delayTime);
         }
-    };
-    
-    appendList();
+    }
 
-    /* ajaxでの遷移時にフックする */
+   //appendList();
+
     $.pageUpdate(appendList);
-
-};
-
-location.href = 'javascript:(' + script.toString() + ')()';
-
+});
